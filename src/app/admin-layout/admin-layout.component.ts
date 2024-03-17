f547ffffe92d3ae977647b1f053f3aa21aa41ca4
Satrughna <thisissatrughna@gmail.com>
2024-03-17 18:17:27 +0530

    preparing layout

--- src/app/admin-layout/admin-layout.component.ts
+++ src/app/admin-layout/admin-layout.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [DashboardComponent, RouterOutlet],
+  imports: [
+    DashboardComponent,
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+  ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
 })
---
+++ src/app/components/components.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-components',
+  standalone: true,
+  imports: [],
+  templateUrl: './components.component.html',
+  styleUrl: './components.component.scss'
+})
+export class ComponentsComponent {
+
+}
