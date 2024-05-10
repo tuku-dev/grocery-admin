a201da3ca920458e45209a832bb6fa281fda1d8f
Satrughna <thisissatrughna@gmail.com>
2024-05-10 23:15:53 +0530

    WIP form submit

--- src/app/admin-layout/product/add/add.component.ts
+++ src/app/admin-layout/product/add/add.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss',
 })
-export class AddComponent {}
+export class AddComponent implements OnInit {
+  addProductForm: FormGroup | any;
+  submitted = false;
+  formData: any;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.addProductForm = this.fb.group({
+      productName: ['', [Validators.required]],
+      tags: [''],
+      description: [''],
+    });
+  }
+
+  get f() {
+    return this.addProductForm.controls;
+  }
+
+  formSubmit() {
+    console.log(this.addProductForm.value);
+    this.submitted = true;
+
+    if (this.addProductForm.invalid) return;
+
+    this.formData = this.addProductForm.value;
+    console.log(this.formData);
+  }
+}
